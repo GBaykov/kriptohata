7a800f2d6948f32d8712c04acc08754ccc92877e
GBaykov <gleb.baykov99@gmail.com>
2022-09-26 23:41:50 +0300

    feat: change favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './store/reducers/UserSlice';
