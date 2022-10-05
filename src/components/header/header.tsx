7547496092b877912abe2b081ce62e6fa96d505b
GBaykov <gleb.baykov99@gmail.com>
2022-10-06 00:32:23 +0300

    style: add some style to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,44 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
   return (
     <header className="header">
-      <Link to="/" className="">
-        Домой
-      </Link>
-      <Link to="/videocards" className="header-videocards header-item">
-        Видеокарты
-      </Link>
-      <Link to="/newminers" className="header-newminers header-item">
-        Майнеры новые
-      </Link>
-      <Link to="/oldminers" className="header-oldminers header-item">
-        Майнеры Б/У
-      </Link>
-      <Link to="/howorder" className="header-oldminers header-item">
-        Как заказать
-      </Link>
+      <div className="header__top top">
+        <div className="header-wrapper">
+          <div className="top__local-choices choices">
+            <div className="choices-country choice">
+              Страна
+              <select name="country">
+                <option value="1">Россия</option>
+                <option value="2">Беларусь</option>
+              </select>
+            </div>
+            <div className="choices-currency choice">
+              Валюта
+              <select name="currency">
+                <option value="1">Рубли</option>
+                <option value="2">Бел.руб</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header__search"></div>
+      <div className="header-wrapper">
+        <NavLink to="/videocards" className="header-videocards header-item">
+          Видеокарты
+        </NavLink>
+        <NavLink to="/newminers" className="header-newminers header-item">
+          Майнеры новые
+        </NavLink>
+        <NavLink to="/oldminers" className="header-oldminers header-item">
+          Майнеры Б/У
+        </NavLink>
+        <NavLink to="/howorder" className="header-oldminers header-item">
+          Как заказать
+        </NavLink>
+      </div>
     </header>
   );
 };
