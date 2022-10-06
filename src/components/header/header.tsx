a18319cbcc18af7a18f0241f48855f3a69764c60
GBaykov <gleb.baykov99@gmail.com>
2022-10-07 01:04:26 +0300

    feat: add html without css - header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+  };
+
   return (
     <header className="header">
-      <div className="header__top top">
-        <div className="header-wrapper">
+      <div className="header-wrapper header-content">
+        <div className="header__top top">
           <div className="top__local-choices choices">
             <div className="choices-country choice">
               Страна
@@ -22,22 +29,51 @@ const Header = () => {
               </select>
             </div>
           </div>
+          <div className="header-right right">
+            <p className="right-phone1">+7-926-953-02-69</p>
+            <p className="right-phone2">+375-25-654-23-66</p>
+            <p className="right-backcall">Обратный звонок</p>
+            <div className="right-messengers mess">
+              <p className="mess-images">
+                <img className="insta" src="insta.svg" alt="" />
+                <img className="viber" src="viber.svg" alt="" />
+              </p>
+            </div>
+            <p className="right-accaunt">
+              <img src="user.svg" alt="" />
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="header__search"></div>
-      <div className="header-wrapper">
-        <NavLink to="/videocards" className="header-videocards header-item">
-          Видеокарты
-        </NavLink>
-        <NavLink to="/newminers" className="header-newminers header-item">
-          Майнеры новые
-        </NavLink>
-        <NavLink to="/oldminers" className="header-oldminers header-item">
-          Майнеры Б/У
-        </NavLink>
-        <NavLink to="/howorder" className="header-oldminers header-item">
-          Как заказать
-        </NavLink>
+        <div className="header-center">
+          <p className="header-logo">
+            <img src="image.svg" alt="header-logo" />
+            КриптоХата
+          </p>
+          <div className="header-searchbar">
+            <input type="text" onChange={onInputChange} value={inputValue} />
+          </div>
+          <div className="header-chosen">
+            <img src="star.svg" alt="" />
+          </div>
+          <div className="header-basket">
+            <img src="basket.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="header-nav">
+          <NavLink to="/videocards" className="header-videocards header-item">
+            Видеокарты
+          </NavLink>
+          <NavLink to="/newminers" className="header-newminers header-item">
+            Майнеры новые
+          </NavLink>
+          <NavLink to="/oldminers" className="header-oldminers header-item">
+            Майнеры Б/У
+          </NavLink>
+          <NavLink to="/howorder" className="header-oldminers header-item">
+            Как заказать
+          </NavLink>
+        </div>
       </div>
     </header>
   );
