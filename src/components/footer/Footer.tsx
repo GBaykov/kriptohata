a892ba6bd73bd9460dbb6a39e1c718e21b429da9
GBaykov <gleb.baykov99@gmail.com>
2022-11-06 17:22:33 +0300

    fix: fix background-colors

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../button';
 import './footer.css';
 
 function Footer() {
@@ -6,13 +7,14 @@ function Footer() {
     <footer className="footer">
       <div className="footer-wrapper">
         <div className="footer__content content">
-          <p className="content-logo">
-            <img className="content-logo-img" src="image.svg" alt="footer-logo" />
-            <span className="content-logo-text">
-              Крипто<b>Хата</b>
-            </span>
-          </p>
-
+          <div className="footer__content-logo">
+            <p className="content-logo">
+              <img className="content-logo-img" src="image.svg" alt="footer-logo" />
+              <span className="content-logo-text">
+                Крипто<b>Хата</b>
+              </span>
+            </p>
+          </div>
           <div className="content__phones phones">
             <a className="phones-phone" href="tel:+79269530269">
               +7-926-953-02-69
@@ -41,8 +43,13 @@ function Footer() {
             </p>
           </div>
           <div className="content__buttons buttons">
-            <button className="buttons-register"></button>
-            <button className="buttons-login"></button>
+            <Button isMain={true} text="Зарегистрироваться" />
+            <p>
+              {' '}
+              <Button isMain={false} text="Войти" />
+            </p>
+            {/* <button className="buttons-register"></button>
+            <button className="buttons-login"></button> */}
           </div>
         </div>
         <div className="footer__copyright copyright">
